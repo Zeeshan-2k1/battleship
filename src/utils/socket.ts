84b6280bdf97f063a544c0f9070886f2b1aaaf80
Zeeshan Ashraf <zeeshan.a@drinkprime.in>
2024-02-24 11:27:40 +0530

    fix: set react app env vars for server

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 
-export const SERVER_URL = process.env.SERVER_URL ?? 'http:/localhost:3333';
+export const SERVER_URL =
+  process.env.REACT_APP_SERVER_URL ?? 'http://localhost:3333';
 
 export const socketBuilder = ({
   roomId,
